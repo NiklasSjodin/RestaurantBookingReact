8963d39295550537876342eeb96c4fb445947dfc
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-10 22:21:30 +0200

    App.jsx

---
+++ RestaurantBookingReact/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ReservationForm from './Components/Reservation'
+
+function App() {
+
+  return (
+    <>
+      <ReservationForm/>
+    </>
+  )
+}
+
+export default App
